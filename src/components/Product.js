71a8c3d4bb4ff6b0d1901b01a41fa25c7a170e68
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-09-09 23:28:59 -0400

    making api calls with axios to backend server

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 function Product({product}) {
   return (
     <Card className="my-3 p-2 rounded">
-        <Link to={'/product/3'}> 
+        <Link to={`/product/${product._id}`}> 
             <Card.Img src={product.image} />
         </Link>
         <Card.Body>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios' 
 
-function HomeScreen() {
+import products from '../products'
+
+function HomeScreen() { 
+
+  const [products, setProducts] = useState([]) 
+
+  useEffect(()=>{
+    async function fetchProducts(){
+      const {data} = await axios.get('http://localhost:8000/api/products/')
+      setProducts(data)
+    }
+    fetchProducts()     
+  },[]); 
+
   return (
     <div>
       <h1 className="text-muted">Latest Products</h1> 
       <Row>
-        {products.map((product) =>(
+        {products.map((product) =>( 
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} /> 
             </Col> 
