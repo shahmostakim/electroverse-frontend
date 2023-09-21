c6ff9e7db3c41201eae7b1c24df9f11fadfd9148
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-09-21 17:13:59 -0400

    Working on checkout page
    Counts total cart items, shows total price adding all items price with Qty.
    
    Task pending: Need to implement "delete item" action firing upon clicking on remove item button

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -5,12 +5,12 @@ export const cartReducer = (state={cartItems:[]}, action)=>{
     switch (action.type){
         case CART_ADD_ITEM: 
             const item = action.payload
-            const existItem = state.cartItems.find(x=>x.product===item.product)
+            const existItem = state.cartItems.find(x=>x.productId===item.productId)
             if(existItem){ // item exists in cart already 
                 return{
                     ...state,
                     /* replaces the existing cart item with the updated item coming from action payload */
-                    cartItems: state.cartItems.map(x=>x.product===existItem.product?item:x)
+                    cartItems: state.cartItems.map(x=>x.productId===existItem.productId?item:x)
                 } 
             }else{ // item was not added to cart before 
                 return{
--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,11 +1,123 @@
 
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
-function CartScreen() {
+
+function CartScreen({match, location, history}) {
+
+    const Id = match.params.id 
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    //console.log('qty: ', qty)
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state=>state.cart)
+    const {cartItems} = cart 
+
+    // function for remove item button
+    const removeFromCartHandler = (id)=>{
+        console.log('remove: ', id) 
+    }
+
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping')
+    }
+
+    // calculate number of total cart items 
+    const NumberOfCartItems = (cartItems) => {
+        let total = cartItems.reduce((acc, item)=>acc+item.qty, 0)
+        if (total>0){
+            return false
+        }else{
+            return true 
+        }
+    }
+
+    useEffect(()=>{
+        if(Id){
+            dispatch(addToCart(Id, qty)) 
+        }
+    }, [dispatch, Id, qty])
+
+
   return (
-    <div>
-      Cart 
-    </div>
+    <Row>
+        <Col md={8}>
+            <h1>Shopping Cart</h1>
+            { cartItems.length === 0 ? (
+                <Message variant='info'>
+                    Your Cart is empty <Link to='/'>Go Back</Link>
+                </Message>
+            ) : (
+                <ListGroup variant='flush'>
+                    {cartItems.map(item=>(
+                        <ListGroup.Item key={item.productId}>
+                            <Row>
+                                <Col md={2}><Image src={item.image} alt={item.name} fluid rounded /></Col>
+                                <Col md={3}><Link to={`/product/${item.productId}`}>{item.name}</Link></Col>
+                                <Col md={2}>${item.price}</Col>
+                                <Col md={3}>
+                                    <Form.Control 
+                                        as="select"
+                                        value={item.qty}
+                                        onChange={(e)=>dispatch(addToCart(item.productId, Number(e.target.value)))}
+                                    >
+                                    {
+                                        /* for each cart item, populate individual dynamic dropdown lists based on each product's countInStock value */
+                                        /* similar to productScreen page, but here we do the same for each diffrent products in cartItems array */
+                                        [...Array(item.countInStock).keys()].map((x)=>(
+                                        <option key={x+1} value={x+1}>
+                                            {x+1} 
+                                        </option>  
+                                        ))
+                                        
+                                        //qtyDropdownOptions // alternate way to populate dropdown options, does the same thing as the code above 
+                                    }
+                                    </Form.Control>
+                                </Col>
+                                <Col md={1}>
+                                    <Button
+                                        type='button'
+                                        variant='outline-danger'
+                                        onClick={()=>removeFromCartHandler(item.productId)}
+                                    >
+                                        <i className='fas fa-trash'></i>
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            )}
+        </Col>
+        <Col md={4}>
+            <Card>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h2>Subtotal ({cartItems.reduce((acc, item)=>acc+item.qty, 0)}) items</h2>
+                        ${cartItems.reduce((acc, item)=>acc+item.qty*item.price, 0).toFixed(2)}
+                    </ListGroup.Item>
+                </ListGroup>
+                <ListGroup.Item>
+                    <Row className='p-2 mx-2'>
+                        <Button
+                            type='button'
+                            variant='info'
+                            /*disabled={NumberOfCartItems(cartItems)}*/
+                            disabled={cartItems.length===0} /* after refactoring */
+                            onClick={checkoutHandler}
+                        >
+                            Proceed To Checkout 
+                        </Button>
+                    </Row>
+                </ListGroup.Item>
+            </Card>
+        </Col>
+    </Row>
   )
 }
 
