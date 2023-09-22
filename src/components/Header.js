97c442f0cc322f1adeb978c312ebe7b03029adc1
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-09-21 20:05:12 -0400

    Customized "cart" menu button
    If cart is not empty, cart menu link shows the number beside for easy visualization of number of items in the cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector } from 'react-redux'
 
 function Header() {
+
+    const {cartItems} = useSelector(state=>state.cart)
+
+
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -14,7 +19,7 @@ function Header() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <LinkContainer to="/cart">
-                            <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                            <Nav.Link><i className="fas fa-shopping-cart"></i> Cart{cartItems.length>0?('('+cartItems.length+')'):''}</Nav.Link>
                         </LinkContainer> 
                         <LinkContainer to="/login">
                             <Nav.Link><i className="fas fa-user"></i> Login</Nav.Link>
