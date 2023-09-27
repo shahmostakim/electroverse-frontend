f64af2f996a4d8271fe9d31d454ea039dd052e39
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-09-27 10:04:24 -0400

    User Register complete
    Implemented User Register feature, updated userReducer, userActions, updated redux store, added "Register User" form in RegisterScreen, updated App.js with user registration link

---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,111 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
+import { register } from '../actions/userActions'
+
+function RegisterScreen({location, history}) {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('') 
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('') 
+
+    const dispatch = useDispatch() 
+
+    const redirect = location.search ? location.search.split('=')[1] : '/' 
+
+    const userRegister = useSelector(state=>state.userRegister) 
+    const {error, loading, userInfo} = userRegister 
+
+    useEffect(()=>{
+        if(userInfo){ // if user is already logged in 
+            history.push(redirect) // user cannot see the login page, if logged in 
+        }
+    }, [history, userInfo, redirect]) 
+
+    const submitHandler = (e) => {
+        e.preventDefault() 
+        if(password != confirmPassword){
+            setMessage('Passwords do not match')
+        }else{
+            dispatch(register(name, email, password))
+        }
+         
+        //if(error){console.log(error)}
+    }
+
+  return (
+    <FormContainer> 
+      <h1>Sign In </h1>
+        {message && (<Message variant='danger'>{message}</Message>)}
+        {error && (<Message variant='danger'>{error.message}</Message>)}
+        {loading && (<Loader />)} 
+        <Form onSubmit={submitHandler}>
+
+            <Form.Group controlId='name'> 
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    required 
+                    type='name'
+                    placeholder='Enter name'
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)} 
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='email'> 
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                    required
+                    type='email'
+                    placeholder='Enter email'
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)} 
+                ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='password'> 
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    required
+                    type='password'
+                    placeholder='Enter password'
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}  
+                ></Form.Control> 
+            </Form.Group>
+
+            <Form.Group controlId='passwordConfirm'> 
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                    required
+                    type='password'
+                    placeholder='Confirm password'
+                    value={confirmPassword}
+                    onChange={(e)=>setConfirmPassword(e.target.value)}  
+                ></Form.Control> 
+            </Form.Group>
+
+            <Row className='py-3'>
+                <Col>
+                    <Button type='submit' variant='primary'>Register</Button> 
+                </Col>
+            </Row>
+
+        </Form>
+
+        <Row className='py-3'>
+            <Col>
+                Have an account? <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>Login</Link>
+            </Col>
+        </Row>
+    </FormContainer>
+  )
+}
+
+export default RegisterScreen
