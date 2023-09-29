e5527953ad198132b06208c912d614fdaa9aab92
Shah Mostakim <shah.md.mostakim@gmail.com>
2023-09-29 15:34:19 -0400

    Checkout - Progress Bar
    Added progress bar (checkout steps) component to clearly track different steps in checkout process. User can go back and forth between steps to change data before placing final order

---
+++ src/components/CheckoutSteps.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+function CheckoutSteps({step1, step2, step3, step4}){
+
+
+
+  return (
+    <Nav claasName="justify-content-center mb-4">
+        <Nav.Item>
+            {step1 ? (
+                <LinkContainer to='/login'>
+                    <Nav.Link>Login &gt;</Nav.Link> 
+                </LinkContainer>
+            ) : (
+                <Nav.Link disabled>Login &gt;</Nav.Link>
+            )}
+        </Nav.Item>
+
+        <Nav.Item>
+            {step2 ? (
+                <LinkContainer to='/shipping'>
+                    <Nav.Link>Shipping &gt;</Nav.Link>
+                </LinkContainer>
+            ) : (
+                <Nav.Link disabled>Shipping &gt;</Nav.Link>
+            )}
+        </Nav.Item>
+
+        <Nav.Item>
+            {step3 ? (
+                <LinkContainer to='/payment'>
+                    <Nav.Link>Payment &gt;</Nav.Link>
+                </LinkContainer>
+            ) : (
+                <Nav.Link disabled>Payment &gt;</Nav.Link>
+            )}
+        </Nav.Item>
+
+        <Nav.Item>
+            {step4 ? (
+                <LinkContainer to='/placeorder'>
+                    <Nav.Link>Place Order</Nav.Link>
+                </LinkContainer>
+            ) : (
+                <Nav.Link disabled>Place Order</Nav.Link>
+            )}
+        </Nav.Item>
+      
+    </Nav>
+  )
+}
+
+export default CheckoutSteps
